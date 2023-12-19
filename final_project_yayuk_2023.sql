f4de000bd606249c396665b06577741f278c5fe7
Firmansyah Mukti Wijaya <iki.mukti@gmail.com>
2023-12-19 10:14:46 +0700

    dsaffaf

--- final_project_yayuk_2023.sql
+++ final_project_yayuk_2023.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 12, 2023 at 01:42 PM
+-- Generation Time: Dec 19, 2023 at 04:09 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -55,22 +55,24 @@ INSERT INTO `Ingredients` (`IngredientID`, `IngredientName`, `PurchasePrice`, `Q
 (1, 'Timun Segar', 7000.00, 5.00, 20, 1, 3500.00, 7, 'Supplier A', 'Timun segar berkualitas', 2, 'Cool Storage', 'kg', 0.50, 15.00, 500.00, 50.00),
 (2, 'Tepung Ayam', 19000.00, 1.00, 30, 1, 380.00, 182, 'Supplier B', 'Tepung ayam khusus untuk pencelupan', 3, 'Dry Storage', 'kg', 3.00, 90.00, 1000.00, 2.00),
 (3, 'Marinasi Spesial', 2000.00, 1.00, 30, 1, 80.00, 730, 'Supplier C', 'Bumbu ayam spesial', 3, 'Dry Storage', 'piece', 6.00, 180.00, 2000.00, 4.00),
-(4, 'Sambal', 42000.00, 1.00, 300, 1, 2000.00, 90, 'Supplier D', 'Saus pedas spesial', 3, 'Cool Storage', 'kg', NULL, NULL, NULL, NULL),
-(5, 'Nasi Putih', 10000.00, 1.00, 25, 1, 3000.00, 1, 'Supplier E', 'Nasi putih matang', 10, 'Warm Storage', 'kg', NULL, NULL, NULL, NULL),
-(6, 'Kantong Plastik Kecil', 200.00, 1.00, 100, 1, 20.00, 0, 'Supplier F', 'Kantong plastik kecil berkualitas', 500, 'Dry Storage', 'pack', NULL, NULL, NULL, NULL),
-(7, 'Kresek Plastik Kecil', 7500.00, 1.00, 100, 1, 500.00, 0, 'Supplier G', 'Kresek plastik kecil berkualitas', 2, 'Dry Storage', 'pack', NULL, NULL, NULL, NULL),
-(8, 'Foam Kecil (Wadah)', 18000.00, 1.00, 100, 1, 100.00, NULL, 'Supplier H', 'Wadah foam kecil berkualitas', 100, 'Dry Storage', 'pack', NULL, NULL, NULL, NULL),
-(9, 'Foam Besar (Wadah)', 28000.00, 1.00, 100, 1, 150.00, NULL, 'Supplier I', 'Wadah foam besar berkualitas', 50, 'Dry Storage', 'pack', NULL, NULL, NULL, NULL),
-(10, 'Cup Minuman Kecil', 17000.00, 1.00, 100, 1, 30.00, NULL, 'Supplier J', 'Cup minuman kecil berkualitas', 200, 'Dry Storage', 'pack', NULL, NULL, NULL, NULL),
-(11, 'Cup Minuman Besar', 22000.00, 1.00, 100, 1, 50.00, NULL, 'Supplier K', 'Cup minuman besar berkualitas', 150, 'Dry Storage', 'piece', NULL, NULL, NULL, NULL),
-(12, 'Ayam', 53000.00, 1.00, 30, 1, 2000.00, 365, 'Supplier A', 'Ayam Potong', 3, 'Cool Storage', 'kg', NULL, NULL, NULL, NULL),
-(13, 'Kresek Plastik Besar', 25000.00, 1.00, 50, 1, 500.00, 0, 'Suplier X', 'Kresek Plastik Besar Yahud', 1, 'Dry Storage', 'pack', NULL, NULL, NULL, NULL),
-(14, 'Kresek Plastik Sedang', 8000.00, 1.00, 100, 1, 500.00, 0, 'Suplier X', 'Kresek Plastik Sedang dang dang dang', 1, 'Dry Storage', 'pack', NULL, NULL, NULL, NULL),
-(15, 'Minyak Goreng', 75000.00, 1.00, 210, 1, 2000.00, 730, 'Suplier M', 'Minyak Kelapa Sawit Murni', 10, 'Dry Storage', 'kg', NULL, NULL, NULL, NULL),
-(16, 'Gas Elpigi 15Kg', 275000.00, 1.00, 2700, 1, 2000.00, 30, 'Suplier G', 'Gas Elpigi 15Kg Gas Negoro', 2, 'Dry Storage', 'kg', NULL, NULL, NULL, NULL),
-(17, 'Teh Minang', 5000.00, 1.00, 30, 1, 500.00, 365, 'Supplier T', 'Teh Minang Kabau Asli', 6, 'Dry Storage', 'piece', NULL, NULL, NULL, NULL),
-(18, 'Gula Pasir', 16500.00, 1.00, 80, 1, 1000.00, 365, 'Suplier G', 'Gula Pasir Pabrik Gula Mrican', 4, 'Dry Storage', 'kg', NULL, NULL, NULL, NULL),
-(19, 'Air Galon ', 4000.00, 1.00, 54, 1, 500.00, 7, 'Suplier G', 'Banyu Sri Sejahtera Sedudo', 2, 'Dry Storage', 'liter', NULL, NULL, NULL, NULL);
+(4, 'Sambal', 42000.00, 1.00, 300, 1, 4200.00, 90, 'Supplier D', 'Saus pedas spesial', 4, 'Cool Storage', 'kg', 0.33, 10.00, 4000.00, 10.00),
+(5, 'Nasi Putih', 10000.00, 1.00, 25, 1, 1000.00, 1, 'Supplier E', 'Nasi putih matang', 13, 'Warm Storage', 'kg', 3.00, 90.00, 1000.00, 10.00),
+(6, 'Kantong Plastik Kecil', 10000.00, 1.00, 100, 1, 200.00, 0, 'Supplier F', 'Kantong plastik kecil berkualitas', 9, 'Dry Storage', 'pack', 0.50, 15.00, 500.00, 2.00),
+(7, 'Kresek Plastik Kecil', 7500.00, 1.00, 100, 1, 150.00, 365, 'Supplier G', 'Kresek plastik kecil berkualitas', 10, 'Dry Storage', 'pack', 0.50, 15.00, 500.00, 2.00),
+(8, 'Foam Kecil (Wadah)', 23000.00, 1.00, 100, 1, 460.00, 365, 'Supplier H', 'Wadah foam kecil berkualitas', 11, 'Dry Storage', 'pack', 1.00, 30.00, 1000.00, 2.00),
+(9, 'Foam Besar (Wadah)', 25000.00, 1.00, 100, 1, 500.00, 365, 'Supplier I', 'Wadah foam besar berkualitas', 11, 'Dry Storage', 'pack', 1.00, 30.00, 1000.00, 2.00),
+(10, 'Cup Minuman Kecil', 12000.00, 1.00, 100, 1, 240.00, 365, 'Supplier J', 'Cup minuman kecil berkualitas', 22, 'Dry Storage', 'pack', 2.00, 60.00, 1000.00, 2.00),
+(11, 'Cup Minuman Besar', 13000.00, 1.00, 100, 1, 260.00, 365, 'Supplier K', 'Cup minuman besar berkualitas', 21, 'Dry Storage', 'piece', 2.00, 60.00, 1000.00, 2.00),
+(12, 'Ayam', 53000.00, 1.00, 30, 1, 5300.00, 365, 'Supplier A', 'Ayam Potong', 13, 'Cool Storage', 'kg', 3.00, 90.00, 5000.00, 10.00),
+(13, 'Kresek Plastik Besar', 25000.00, 1.00, 50, 1, 500.00, 365, 'Suplier X', 'Kresek Plastik Besar Yahud', 5, 'Dry Storage', 'pack', 0.20, 6.00, 1000.00, 2.00),
+(14, 'Kresek Plastik Sedang', 8000.00, 1.00, 100, 1, 160.00, 365, 'Suplier X', 'Kresek Plastik Sedang dang dang dang', 11, 'Dry Storage', 'pack', 0.33, 10.00, 1000.00, 2.00),
+(15, 'Minyak Goreng', 75000.00, 1.00, 210, 1, 1500.00, 730, 'Suplier M', 'Minyak Kelapa Sawit Murni', 5, 'Dry Storage', 'kg', 0.33, 10.00, 2000.00, 2.00),
+(16, 'Gas Elpigi 15Kg', 275000.00, 1.00, 2700, 1, 5500.00, 750, 'Suplier G', 'Gas Elpigi 15Kg Gas Negoro', 1, 'Dry Storage', 'kg', 0.03, 1.00, 3000.00, 2.00),
+(17, 'Teh Minang', 5000.00, 1.00, 30, 1, 5000.00, 365, 'Supplier T', 'Teh Minang Kabau Asli', 60, 'Dry Storage', 'piece', 3.00, 90.00, 2000.00, 100.00),
+(18, 'Gula Pasir', 16500.00, 1.00, 80, 1, 660.00, 365, 'Suplier G', 'Gula Pasir Pabrik Gula Mrican', 12, 'Dry Storage', 'kg', 1.50, 45.00, 1000.00, 4.00),
+(19, 'Air Galon ', 5000.00, 1.00, 54, 1, 2500.00, 7, 'Suplier G', 'Banyu Sri Sejahtera Sedudo', 2, 'Dry Storage', 'liter', 2.00, 60.00, 100.00, 50.00),
+(20, 'Es Batu', 10000.00, 1.00, 50, 1, NULL, 2, '0', 'Pemasok K Es batu', 2, '0', 'pack', 1.00, 30.00, 200.00, 50.00),
+(21, 'Saos Sachet', 10000.00, 2.00, 100, 1, NULL, 365, '0', 'Pemasok S', 24, '0', 'pack', 2.00, 60.00, 1000.00, 2.00);
 
 -- --------------------------------------------------------
 
@@ -86,6 +88,33 @@ CREATE TABLE `IngredientStocks` (
   `LastUpdateStock` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `IngredientStocks`
+--
+
+INSERT INTO `IngredientStocks` (`StockID`, `IngredientID`, `Quantity`, `QuantityPerServings`, `LastUpdateStock`) VALUES
+(1, 1, 1.00, 100.00, '2023-12-30 17:00:00'),
+(2, 2, 20.00, 600.00, '2023-12-30 17:00:00'),
+(3, 3, 96.00, 2880.00, '2023-12-30 17:00:00'),
+(4, 4, 3.00, 900.00, '2023-12-30 17:00:00'),
+(5, 5, 16.00, 400.00, '2023-12-30 17:00:00'),
+(6, 6, 10.00, 1000.00, '2023-12-30 17:00:00'),
+(7, 7, 9.00, 900.00, '2023-12-30 17:00:00'),
+(8, 8, 12.00, 1200.00, '2023-12-30 17:00:00'),
+(9, 9, 12.00, 1200.00, '2023-12-30 17:00:00'),
+(10, 12, 12.00, 360.00, '2023-12-30 17:00:00'),
+(11, 13, 4.00, 200.00, '2023-12-30 17:00:00'),
+(12, 14, 10.00, 1000.00, '2023-12-30 17:00:00'),
+(13, 15, 4.00, 840.00, '2023-12-30 17:00:00'),
+(14, 16, 0.00, 0.00, '2023-12-30 17:00:00'),
+(15, 17, 4.00, 120.00, '2023-12-30 17:00:00'),
+(16, 18, 12.00, 960.00, '2023-12-30 17:00:00'),
+(17, 19, 3.00, 162.00, '2023-12-30 17:00:00'),
+(18, 20, 2.00, 100.00, '2023-12-30 17:00:00'),
+(19, 21, 15.00, 3000.00, '2023-12-30 17:00:00'),
+(20, 10, 28.00, 2800.00, '2023-12-30 17:00:00'),
+(21, 11, 20.00, 2000.00, '2023-12-30 17:00:00');
+
 -- --------------------------------------------------------
 
 --
@@ -100,6 +129,59 @@ CREATE TABLE `IngredientTransactions` (
   `QuantityPerServings` decimal(10,2) NOT NULL,
   `Timestamp` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `IngredientTransactions`
+--
+
+INSERT INTO `IngredientTransactions` (`TransactionID`, `IngredientID`, `TransactionType`, `Quantity`, `QuantityPerServings`, `Timestamp`) VALUES
+(1, 1, 'In', 3.00, 300.00, '2023-12-18 01:42:01'),
+(2, 1, 'In', 17.00, 1700.00, '2023-12-17 17:00:00'),
+(3, 2, 'In', 95.00, 2850.00, '2023-12-18 01:43:56'),
+(4, 3, 'In', 200.00, 6000.00, '2023-12-18 01:44:23'),
+(5, 4, 'In', 12.00, 3600.00, '2023-12-18 01:44:34'),
+(6, 5, 'In', 93.00, 2325.00, '2023-12-18 01:44:49'),
+(7, 6, 'In', 15.00, 1500.00, '2023-12-18 01:45:01'),
+(8, 7, 'In', 16.00, 1600.00, '2023-12-18 01:45:08'),
+(9, 8, 'In', 33.00, 3300.00, '2023-12-18 01:45:17'),
+(10, 1, 'In', 52.00, 5200.00, '2023-12-18 01:45:24'),
+(12, 9, 'In', 32.00, 3200.00, '2023-12-18 01:47:07'),
+(13, 12, 'In', 95.00, 2850.00, '2023-12-18 01:47:20'),
+(14, 13, 'In', 7.00, 350.00, '2023-12-18 01:47:29'),
+(15, 14, 'In', 12.00, 1200.00, '2023-12-18 01:47:43'),
+(16, 15, 'In', 12.00, 2520.00, '2023-12-18 01:47:51'),
+(17, 16, 'In', 1.00, 2700.00, '2023-12-18 01:48:02'),
+(18, 17, 'In', 86.00, 2580.00, '2023-12-18 01:48:17'),
+(19, 18, 'In', 40.00, 3200.00, '2023-12-18 01:48:36'),
+(20, 19, 'In', 55.00, 2970.00, '2023-12-18 01:48:45'),
+(21, 20, 'In', 55.00, 2750.00, '2023-12-18 02:52:41'),
+(22, 21, 'In', 55.00, 11000.00, '2023-12-18 02:53:01'),
+(23, 20, 'In', 5.00, 250.00, '2023-12-17 17:00:00'),
+(24, 10, 'In', 75.00, 7500.00, '2023-12-18 02:55:02'),
+(25, 11, 'In', 60.00, 6000.00, '2023-12-18 02:55:08'),
+(26, 1, 'Out', 18.00, 1800.00, '2024-01-30 17:00:00'),
+(27, 2, 'Out', 20.00, 600.00, '2023-12-30 17:00:00'),
+(28, 2, 'Out', 55.00, 1650.00, '2023-12-30 17:00:00'),
+(29, 3, 'Out', 104.00, 3120.00, '2023-12-30 17:00:00'),
+(30, 6, 'Out', 5.00, 500.00, '2023-12-30 17:00:00'),
+(31, 7, 'Out', 7.00, 700.00, '2023-12-30 17:00:00'),
+(32, 8, 'Out', 21.00, 2100.00, '2023-12-30 17:00:00'),
+(33, 9, 'Out', 22.00, 2200.00, '2023-12-30 17:00:00'),
+(34, 12, 'Out', 83.00, 2490.00, '2023-12-30 17:00:00'),
+(35, 13, 'Out', 3.00, 150.00, '2023-12-30 17:00:00'),
+(36, 14, 'Out', 2.00, 200.00, '2023-12-30 17:00:00'),
+(37, 15, 'Out', 8.00, 1680.00, '2023-12-30 17:00:00'),
+(38, 16, 'Out', 1.00, 2700.00, '2023-12-30 17:00:00'),
+(39, 17, 'Out', 82.00, 2460.00, '2023-12-30 17:00:00'),
+(40, 18, 'Out', 28.00, 2240.00, '2023-12-30 17:00:00'),
+(41, 19, 'Out', 52.00, 2808.00, '2023-12-30 17:00:00'),
+(42, 20, 'Out', 58.00, 2900.00, '2023-12-30 17:00:00'),
+(43, 21, 'Out', 40.00, 8000.00, '2023-12-30 17:00:00'),
+(44, 10, 'Out', 47.00, 4700.00, '2023-12-30 17:00:00'),
+(45, 11, 'Out', 40.00, 4000.00, '2023-12-30 17:00:00'),
+(46, 4, 'Out', 9.00, 2700.00, '2023-12-30 17:00:00'),
+(47, 5, 'Out', 77.00, 1925.00, '2023-12-30 17:00:00'),
+(48, 1, 'Out', 1.00, 100.00, '2023-12-30 17:00:00');
 
 -- --------------------------------------------------------
 
@@ -123,7 +205,30 @@ INSERT INTO `LogActivities` (`LogID`, `UserID`, `ActivityDescription`, `Activity
 (2, 137648118, 'Ingredient with Name: Tepung Ayam has been updated.', '2023-12-12 11:57:29'),
 (3, 137648118, 'Ingredient with Name: Timun Segar has been updated.', '2023-12-12 12:37:59'),
 (4, 137648118, 'Ingredient with Name: Tepung Ayam has been updated.', '2023-12-12 12:40:51'),
-(5, 137648118, 'Ingredient with Name: Marinasi Spesial has been updated.', '2023-12-12 12:41:52');
+(5, 137648118, 'Ingredient with Name: Marinasi Spesial has been updated.', '2023-12-12 12:41:52'),
+(6, 137648118, 'User logged in', '2023-12-18 01:41:32'),
+(7, 137648118, 'Bahan dengan Nama: Es Batu telah dibuat dengan detail berikut - Harga Beli: 10000, Jumlah per Beli: 1, Porsi per Bahan: 50.', '2023-12-18 02:33:09'),
+(8, 137648118, 'Ingredient with Name: Timun Segar has been updated.', '2023-12-18 02:34:16'),
+(9, 137648118, 'Ingredient with Name: Tepung Ayam has been updated.', '2023-12-18 02:34:27'),
+(10, 137648118, 'Ingredient with Name: Marinasi Spesial has been updated.', '2023-12-18 02:34:43'),
+(11, 137648118, 'Ingredient with Name: Sambal has been updated.', '2023-12-18 02:35:26'),
+(12, 137648118, 'Ingredient with Name: Nasi Putih has been updated.', '2023-12-18 02:36:50'),
+(13, 137648118, 'Ingredient with Name: Kantong Plastik Kecil has been updated.', '2023-12-18 02:38:32'),
+(14, 137648118, 'Ingredient with Name: Kresek Plastik Kecil has been updated.', '2023-12-18 02:39:57'),
+(15, 137648118, 'Ingredient with Name: Foam Kecil (Wadah) has been updated.', '2023-12-18 02:40:59'),
+(16, 137648118, 'Ingredient with Name: Foam Besar (Wadah) has been updated.', '2023-12-18 02:41:33'),
+(17, 137648118, 'Ingredient with Name: Cup Minuman Kecil has been updated.', '2023-12-18 02:43:09'),
+(18, 137648118, 'Ingredient with Name: Cup Minuman Besar has been updated.', '2023-12-18 02:43:43'),
+(19, 137648118, 'Ingredient with Name: Ayam has been updated.', '2023-12-18 02:44:16'),
+(20, 137648118, 'Ingredient with Name: Kresek Plastik Besar has been updated.', '2023-12-18 02:45:01'),
+(21, 137648118, 'Ingredient with Name: Kresek Plastik Sedang has been updated.', '2023-12-18 02:45:50'),
+(22, 137648118, 'Ingredient with Name: Minyak Goreng has been updated.', '2023-12-18 02:46:32'),
+(23, 137648118, 'Ingredient with Name: Gas Elpigi 15Kg has been updated.', '2023-12-18 02:47:14'),
+(24, 137648118, 'Ingredient with Name: Teh Minang has been updated.', '2023-12-18 02:47:54'),
+(25, 137648118, 'Ingredient with Name: Gula Pasir has been updated.', '2023-12-18 02:48:51'),
+(26, 137648118, 'Ingredient with Name: Air Galon  has been updated.', '2023-12-18 02:50:54'),
+(27, 137648118, 'Bahan dengan Nama: Saos Sachet telah dibuat dengan detail berikut - Harga Beli: 10000, Jumlah per Beli: 2, Porsi per Bahan: 100.', '2023-12-18 02:52:11'),
+(28, 137648118, 'Ingredient with Name: Nasi Putih has been updated.', '2023-12-18 03:55:15');
 
 -- --------------------------------------------------------
 
@@ -216,7 +321,7 @@ CREATE TABLE `Users` (
 
 INSERT INTO `Users` (`UserID`, `Username`, `Password`, `Email`, `FullName`, `DateOfBirth`, `Gender`, `Address`, `PhoneNumber`, `RoleID`, `AccountCreationDate`, `LastLogin`, `AccountStatus`, `ProfilePictureURL`, `ActivationStatus`) VALUES
 (0, 'ikimukti', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', '19103020046@unpkediri.ac.id', 'Firmansyah Mukti Wijaya', '2023-10-12', 'Male', 'Nglaban 1111', '081216318022', 2, '2023-11-25 12:09:14', '2023-10-29 20:04:55', NULL, '653e5a409b4fb.jpeg', NULL),
-(137648118, 'admin', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 'admin@ikimukti.com', 'Administrator', NULL, NULL, NULL, NULL, 1, '2023-12-12 11:32:45', '2023-12-12 18:32:45', NULL, '6562102a33af3.png', NULL);
+(137648118, 'admin', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 'admin@ikimukti.com', 'Administrator', NULL, NULL, NULL, NULL, 1, '2023-12-18 01:41:32', '2023-12-18 08:41:32', NULL, '6562102a33af3.png', NULL);
 
 --
 -- Indexes for dumped tables
@@ -294,25 +399,25 @@ ALTER TABLE `Users`
 -- AUTO_INCREMENT for table `Ingredients`
 --
 ALTER TABLE `Ingredients`
-  MODIFY `IngredientID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `IngredientID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `IngredientStocks`
 --
 ALTER TABLE `IngredientStocks`
-  MODIFY `StockID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `StockID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `IngredientTransactions`
 --
 ALTER TABLE `IngredientTransactions`
-  MODIFY `TransactionID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `TransactionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT for table `LogActivities`
 --
 ALTER TABLE `LogActivities`
-  MODIFY `LogID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `LogID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `ProductIngredients`
@@ -340,32 +445,32 @@ ALTER TABLE `ProductTransactions`
 -- Constraints for table `IngredientStocks`
 --
 ALTER TABLE `IngredientStocks`
-  ADD CONSTRAINT `ingredientstocks_ibfk_1` FOREIGN KEY (`IngredientID`) REFERENCES `Ingredients` (`IngredientID`) ON DELETE CASCADE ON UPDATE NO ACTION;
+  ADD CONSTRAINT `ingredientstocks_ibfk_1` FOREIGN KEY (`IngredientID`) REFERENCES `ingredients` (`IngredientID`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `IngredientTransactions`
 --
 ALTER TABLE `IngredientTransactions`
-  ADD CONSTRAINT `ingredienttransactions_ibfk_1` FOREIGN KEY (`IngredientID`) REFERENCES `Ingredients` (`IngredientID`) ON DELETE CASCADE ON UPDATE NO ACTION;
+  ADD CONSTRAINT `ingredienttransactions_ibfk_1` FOREIGN KEY (`IngredientID`) REFERENCES `ingredients` (`IngredientID`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `LogActivities`
 --
 ALTER TABLE `LogActivities`
-  ADD CONSTRAINT `logactivities_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`);
+  ADD CONSTRAINT `logactivities_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`);
 
 --
 -- Constraints for table `ProductIngredients`
 --
 ALTER TABLE `ProductIngredients`
-  ADD CONSTRAINT `productingredients_ibfk_1` FOREIGN KEY (`ProductID`) REFERENCES `Products` (`ProductID`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `productingredients_ibfk_2` FOREIGN KEY (`IngredientID`) REFERENCES `Ingredients` (`IngredientID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `productingredients_ibfk_1` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `productingredients_ibfk_2` FOREIGN KEY (`IngredientID`) REFERENCES `ingredients` (`IngredientID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `ProductTransactions`
 --
 ALTER TABLE `ProductTransactions`
-  ADD CONSTRAINT `producttransactions_ibfk_1` FOREIGN KEY (`ProductID`) REFERENCES `Products` (`ProductID`) ON DELETE CASCADE ON UPDATE NO ACTION;
+  ADD CONSTRAINT `producttransactions_ibfk_1` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `Users`
